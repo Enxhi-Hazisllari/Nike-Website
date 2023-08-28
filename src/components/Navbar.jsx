b0240f28cf4f9050784e22feb5ccff624db8a0eb
Enxhi Hazisllari <enxhi.hazisllari14@gmail.com>
2023-08-28 14:44:09 +0200

    update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { headerLogo } from "../assets/images";
 import { hamburger } from "../assets/icons";
+import { navLinks } from "../constants";
 
 const Navbar = () => {
   return (
@@ -9,8 +10,25 @@ const Navbar = () => {
           <img src={headerLogo} alt="Logo" width={130} height={29} />
         </a>
         <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
-
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className="font-monts errat leading-normal text-lg text-slate-gray"
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
         </ul>
+        <div className='flex gap-2 text-lg leading-normal font-medium font-montserrat max-lg:hidden wide:mr-24'>
+          <a href='/'>Sign in</a>
+          <span>/</span>
+          <a href='/'>Explore now</a>
+        </div>
+        <div className='hidden max-lg:block'>
+          <img src={hamburger} alt='hamburger icon' width={25} height={25} />
+        </div>
       </nav>
     </header>
   );
